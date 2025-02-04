b4f07e1d940daacf424205e080e61b35f3bef241
Andre Riesco <andre.riesco@toradex.com>
2025-02-04 16:06:40 -0300

    tasks.py: Use bash to execute the VSCode tasks commands and scripts
    use bash to execute the VSCode tasks commands and scripts, as they are
    written and tested in bash.
    
    Signed-off-by: Andre Riesco <andre.riesco@toradex.com>

--- scripts/utils/pip/torizon_templates_utils/tasks.py
+++ scripts/utils/pip/torizon_templates_utils/tasks.py
@@ -913,12 +913,15 @@ class TaskRunner:
             print(f"Parsed Args: {_args}", color=Color.YELLOW)
             print(f"Parsed Command: {_cmd_join}", color=Color.YELLOW)
 
+        # use bash to execute the VSCode tasks commands and scripts, as they
+        # are written and tested in bash
         _ret = subprocess.run(
             [_cmd, *_args] if not _shell else _cmd_join,
             stdout=None,
             stderr=None,
             env=os.environ,
-            shell=_shell
+            shell=_shell,
+            executable="/bin/bash"
         )
 
         # go back to the last cwd
